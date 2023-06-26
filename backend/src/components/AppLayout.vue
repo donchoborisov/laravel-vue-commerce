50c1cdb1bc5ac29e441f42d5b2f5e135229b3f1b
Doncho Borisov <donco_borisov@yahoo.com>
2023-06-26 11:27:51 +0100

    vue files

---
+++ backend/src/components/AppLayout.vue
@@ -0,0 +1,27 @@
+ <template>
+   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+       
+        Part of the layout    
+    
+      </div>
+  
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+        
+      <router-view></router-view>
+  
+        
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
