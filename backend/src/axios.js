3097b37463dcb65f9a5a20608cd904d1c8ee7996
unknown <Administrator@LSST-AD-IT12.londonsst.ac>
2023-07-19 17:09:44 +0100

    show products 1

--- backend/src/axios.js
+++ backend/src/axios.js
@@ -4,16 +4,16 @@
  import axios from "axios";
  import store from "./store";
  import router from "./router/index.js";
- 
+
  const axiosClient = axios.create({
    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
  })
- 
+
  axiosClient.interceptors.request.use(config => {
    config.headers.Authorization = `Bearer ${store.state.user.token}`
    return config;
  })
- 
+
  axiosClient.interceptors.response.use(response => {
    return response;
  }, error => {
@@ -23,5 +23,5 @@
    }
    throw error;
  })
- 
+
  export default axiosClient;
